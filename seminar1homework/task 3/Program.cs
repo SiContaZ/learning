2bab02514fb730cf067cd7657919f2c16154a279
Sergey Batulin <sicontaz@gmail.com>
2022-06-24 08:51:02 +0300

    wark on task 3

---
+++ seminar1homework/task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 6:
+ Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("число чётное");
+}
+else
+{
+    Console.WriteLine("число нечётное");
+}
