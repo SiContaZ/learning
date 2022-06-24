79ec1f8ba6afbb1885723281973ddb231706209f
Sergey Batulin <sicontaz@gmail.com>
2022-06-24 09:00:18 +0300

    work on task 4

--- seminar1homework/task 4/Program.cs
+++ seminar1homework/task 4/Program.cs
@@ -5,3 +5,11 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int numberN = int.Parse(numberStr);
+
+for (int i = 2; i <= numberN; i += 2)
+{
+    Console.WriteLine(i);
+}
