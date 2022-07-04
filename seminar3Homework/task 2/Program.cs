0131c171817a68fd490807c68f11cd9de8cfdb77
Sergey Batulin <sicontaz@gmail.com>
2022-07-04 14:03:06 +0300

    work on task 2

--- seminar3Homework/task 2/Program.cs
+++ seminar3Homework/task 2/Program.cs
@@ -6,4 +6,38 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53
+AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
 */
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели неверное число. Введите корректное число.";
+
+    while(true)
+    {
+        
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+
+
+int ax = GetNumber("Введите координату x точки a");
+int bx = GetNumber("Введите координату x точки b");
+int ay = GetNumber("Введите координату y точки a");
+int by = GetNumber("Введите координату y точки b");
+int az = GetNumber("Введите координату z точки a");
+int bz = GetNumber("Введите координату z точки b");
+
+double length = Math.Sqrt(Math.Pow(bx - ax,2) + Math.Pow(by - ay,2) + Math.Pow(bz - az,2));
+Console.WriteLine($"Расстояние между точками равняется {length}");
--- seminar3Homework/task 3/Program.cs
+++ seminar3Homework/task 3/Program.cs
@@ -6,3 +6,46 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+    }
+
+    return result;
+}
+
+void Sqrt(int number)
+{   
+    if(number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            Console.WriteLine(Math.Pow(i,3));
+        }
+    }
+    else
+    {
+        for (int i = 1; i >= number; i--)
+        {
+            Console.WriteLine(Math.Pow(i,3));
+        }
+    }
+}
+
+int number = GetNumber("Введите число");
+
+Sqrt(number);
