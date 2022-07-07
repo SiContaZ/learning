5ac2e3c6b9954f9681cfa4f1b71871e951709a1e
Sergey Batulin <sicontaz@gmail.com>
2022-07-07 18:57:32 +0300

    work on task 3

--- seminar4Homework/task 3/Program.cs
+++ seminar4Homework/task 3/Program.cs
@@ -7,87 +7,38 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
+//ручной ввод массива
 
-// массив из заданного восьмизначного числа
-
-/*int GetNumber(string message)
+int GetNumber(string message)
 {
     int result = 0;
-    string errorMessage = "Вы ввели не число. Введите корректное число.";
-
+    Console.WriteLine(message);
     while(true)
     {
-        
-        Console.WriteLine(message);
-
         if(int.TryParse(Console.ReadLine(), out result))
         {
-            if(result == 0)
-            {
-                Console.Clear();
-                Console.WriteLine(errorMessage);
-            }
-            else if(result > 9999999 && result <100000000)
-                break;
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Вы ввели не восьмизначное число число");
-            }
+            break;
         }
         else
         {
             Console.Clear();
-            Console.WriteLine(errorMessage);
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
         }
     }
 
     return result;
 }
-void ArrayNumber(int number)
+void FillArray(int[] array)
 {
-    while(number>0)
+    for (int i = 0; i < array.Length ; i++)
     {
-        Console.Write(number%10 );
-        number/=10;
+        array[i] = GetNumber("Введите число");
+    }
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} ");
     }
 }
+int [] array = new int[8];
 
-int number = GetNumber("Введите восьмизначное число");
-int [] array = new 
-ArrayNumber(number);
-*/
-void PrintArray(int[] array)
-{
-    for(int i = 0; i< array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-}
-
-void PrintArrayForEach(int[] array)
-{
-    foreach(int item in array)
-    {
-        //int item = array[i], в цикле по i 
-
-        Console.Write($"{item} ");
-    }
-}
-
-
-var array = new int[8];
-Random rnd = new Random();
-
-for(int i=0; i < array.Length; i++)
-{
-    array[i] = rnd.Next(1,10);
-}
-
-Console.WriteLine("Вывод через цикл for: ");
-PrintArray(array);
-Console.WriteLine();
-
-
-Console.WriteLine("Вывод через цикл foreach: ");
-PrintArrayForEach(array);
+FillArray(array);
