d10aec86324c6bff00e35f2eec693ddb6acef00e
Sergey Batulin <sicontaz@gmail.com>
2022-07-10 16:46:03 +0300

    work on task 3

--- seminar5Homework/task 3/Program.cs
+++ seminar5Homework/task 3/Program.cs
@@ -1,5 +1,54 @@
 ﻿/*
-Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Задача 38:
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 [3 7 22 2 78] -> 76
 */
+int[] GetArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = randomizer.Next(-100, 101);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void MaxMinDiff(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]>max)
+        {
+            max = array[i];
+        }
+        else if(array[i]<min)
+        {
+            min = array[i];
+        }
+
+    }
+    int diff = max - min;
+    Console.Write($"Максимальный элемент: {max}, минимальный элемент: {min}, разница {diff}.");
+
+}
+Console.WriteLine("Введите размерность массива");
+int dimension = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[] array = GetArray(dimension);
+PrintArray(array);
+Console.WriteLine();
+MaxMinDiff(array);
