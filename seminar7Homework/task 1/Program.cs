b946834b3879f35fea852972041ace128823e0d2
Sergey Batulin <sicontaz@gmail.com>
2022-07-18 11:39:51 +0300

    work on task 1

--- seminar7Homework/task 1/Program.cs
+++ seminar7Homework/task 1/Program.cs
@@ -10,3 +10,56 @@ m = 3, n = 4.
 
 8 7,8 -7,1 9
 */
+
+double [,] InitMatrix(int m, int n)
+{
+    double [,] matrix = new double[m,n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i,j] = Math.Round(randomizer.NextDouble()*20 - 10, 1);
+        }
+    }
+    return matrix;
+}
+
+int GetNumber(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число строк");
+int n = GetNumber("Введите число столбцов");
+double [,] matrix = InitMatrix(m, n);
+Console.WriteLine();
+PrintMatrix(matrix);
