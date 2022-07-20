d245694e3e60449abf9758209717cf22f241485e
Sergey Batulin <sicontaz@gmail.com>
2022-07-20 17:14:40 +0300

    work on task 5

--- seminar8Homework/task 5/Program.cs
+++ seminar8Homework/task 5/Program.cs
@@ -12,3 +12,54 @@
 
 10  9  8 7
 */
+
+int [,] InitMatrix(int m)
+{
+    int [,] matrix = new int[m,m];
+    int buffer = 1;
+    int i = 0;
+    int j = 0;
+    while(buffer<=matrix.GetLength(0)*matrix.GetLength(1))
+    {
+        matrix[i,j] = buffer;
+        buffer++;
+        if(i<=j+1&&i+j<matrix.GetLength(1)-1)
+        {
+            j++;
+        }
+        else if(i<j&&i+j>=matrix.GetLength(0)-1)
+        {
+            i++;
+        }
+        else if(i>=j&&i+j>matrix.GetLength(1)-1)
+        {
+            j--;
+        }
+        else
+        {
+            i--;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+
+int m = 4;
+int[,] matrix = InitMatrix(m);
+PrintMatrix(matrix);
