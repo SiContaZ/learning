6b644b1b590ffc8d6490ed78794c75548cd3ea29
Sergey Batulin <sicontaz@gmail.com>
2022-07-26 13:06:55 +0300

    work on seminar 9

---
+++ seminar9/ex002/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+string recursion(int n, int m)
+{
+    if (n == m)
+        return n.ToString();
+    else
+        return n + ", " + recursion(n + 1, m);
+}
+
+int n = 2;
+int m = 6;
+Console.WriteLine($"{recursion(n, m)}");
---
+++ seminar9/ex003/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+void SumAllNumber(int number, int sum)
+{
+    if (number == 0)
+        Console.WriteLine(sum);
+    else
+    {
+        sum = sum + (number % 10);
+        SumAllNumber(number / 10, sum);
+    }
+}
+
+int m = int.Parse(Console.ReadLine());
+int summ = 0;
+SumAllNumber(m, summ);
