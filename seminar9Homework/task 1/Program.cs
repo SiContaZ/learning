702fbf56420de5b7f8ede62be5a399af27f7851c
Sergey Batulin <sicontaz@gmail.com>
2022-07-26 13:21:30 +0300

    work on task 1

--- seminar9Homework/task 1/Program.cs
+++ seminar9Homework/task 1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+string recursion(int n, int m)
+{
+    if (n == m)
+        return n.ToString();
+    else if (n<m)
+        return recursion(n + 1, m) + ", " + n;
+    else
+        return recursion(n - 1, m) + ", " + n;
+}
+
+int n = 12;
+int m = 6;
+Console.WriteLine($"{recursion(n, m)}");
