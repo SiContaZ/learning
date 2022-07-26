3f73a85a07d2ef820f328cc6aa83e7c9d9e56617
Sergey Batulin <sicontaz@gmail.com>
2022-07-26 13:30:29 +0300

    work on task 2

--- seminar9Homework/task 2/Program.cs
+++ seminar9Homework/task 2/Program.cs
@@ -4,3 +4,26 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+void SumAllNumber(int n,int m, int sum)
+{
+    if (n == m)
+    {
+        sum = sum + n;
+        Console.WriteLine(sum);
+    }
+    else if(n>=m)
+    {
+        sum = sum + n;
+        SumAllNumber(n-1,m, sum);
+    }
+    else
+    {
+        sum = sum + n;
+        SumAllNumber(n+1,m, sum);
+    }
+}
+
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+int summ = 0;
+SumAllNumber(n,m, summ);
